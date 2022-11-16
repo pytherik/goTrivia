9f02aa21c5bd978a646ca38b98a188cd0df5a85b
pytherik <erikberndt@gmx.net>
2022-11-16 14:52:57 +0100

    remove trueFalse todo: routing start

--- public/js/app.js
+++ public/js/app.js
@@ -10,14 +10,16 @@ $(".back").click(() => {
   $(".menu.back").hide();
 })
 
-$("#multiple").click(() => {
-  $(".form-container.multiple").show();
-  $(".form-container.trueFalse").hide();
-})
-$("#single").click(() => {
-  $(".form-container.multiple").hide();
-  $(".form-container.trueFalse").show();
-})
+// $("#multiple").click(() => {
+//   $("#newQuest").hide();
+//   $(".form-container.multiple").show();
+//   $(".form-container.trueFalse").hide();
+// })
+// $("#single").click(() => {
+//   $("#newQuest").hide();
+//   $(".form-container.multiple").hide();
+//   $(".form-container.trueFalse").show();
+// })
 
 function shuffleArray(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
@@ -27,61 +29,31 @@ function shuffleArray(arr) {
   return arr;
 }
 $("#new-quest").click(() => {
-  // $(".answer").css("backgroundColor", "#444");
-  // $(".answer").removeClass("okay");
-  $("#answer1").show();
-  $("#answer3").show();
-
   $(".answer").removeClass("okay yes no");
   $.get("/api/quests", quest => {
-    if (quest.isMultiple) {
-      let answers = quest.wrong_answers;
-      answers.push(quest.right_answer);
-      answers = shuffleArray(answers);
-      console.log(answers);
-      $(".question").html(`<h1>${quest.question}</h1>`)
-      answers.forEach((answer, i) => {
-        $("#answer" + i).text(answer);
-        if (answer == quest.right_answer) {
-          $("#answer" + i).addClass("okay")        }
-        $("#answer" + i).click((e) => {
-          const guess = e.target.innerHTML;
-          if (guess == quest.right_answer) {
-            $("#answer" + i).addClass("yes");
-            // $("#answer" + i).css("backgroundColor", "green");
-            console.log("Richtig");
-          }
-          else {
-            $("#answer" + i).addClass("no");
-            // $("#answer" + i).css("backgroundColor", "red");
-            $(".okay").addClass("yes");
-            // $(".okay").css("backgroundColor", "green");
-            console.log("Falsch");
-          }
-        })
+    let answers = quest.wrong_answers;
+    answers.push(quest.right_answer);
+    answers = shuffleArray(answers);
+    console.log(answers);
+    $(".question").html(`<h1>${quest.question}</h1>`)
+    answers.forEach((answer, i) => {
+      $("#answer" + i).text(answer);
+      if (answer == quest.right_answer) {
+        $("#answer" + i).addClass("okay")
+      }
+      $("#answer" + i).click((e) => {
+        const guess = e.target.innerHTML;
+        if (guess == quest.right_answer) {
+          $("#answer" + i).addClass("yes");
+          console.log("Richtig");
+        }
+        else {
+          $("#answer" + i).addClass("no");
+          $(".okay").addClass("yes");
+          console.log("Falsch");
+        }
       })
-
-    } else {
-      $("#answer1").hide();
-      $("#answer3").hide();
-      $(".question").html(`<h1>${quest.question}</h1>`)
-      $("#answer0").text("Richtig").click(() => {
-        if (quest.isTrue == true) {
-          $("#answer0").addClass("yes")
-        } else {
-          $("#answer0").addClass("no")
-          $("#answer2").addClass("yes")
-        }
-      });
-      $("#answer2").text("Falsch").click(() => {
-        if (quest.isTrue == false) {
-          $("#answer2").addClass("yes")
-        } else {
-          $("#answer0").addClass("no")
-          $("#answer2").addClass("yes")
-        }
-      });
-    }
+    })
   })
 })
 
--- routes/questRoutes.js
+++ routes/questRoutes.js
@@ -9,15 +9,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 router.get('/', async (req, res) => {
   const user = req.session.user;
   const questions = await Quest.find({}).lean();
-  console.log(questions)
   const num = Math.floor(Math.random() * questions.length);
   const randomQuest = questions[num];
-  console.log(randomQuest);
-  if (randomQuest.isMultiple == true) {
-    return res.render('multiple', { user, randomQuest });
-  } else {
-    return res.render('trueFalse', { user, randomQuest });
-  }
+  return res.render('multiple', { user, randomQuest });
 })
 
 router.get('/create', (req, res) => {
@@ -27,29 +21,16 @@ router.get('/create', (req, res) => {
 router.post('/', async (req, res) => {
   const quest = req.body;
   const payload = { author: req.session.user._id }
-  if (quest.multiple) {
-    payload.isMultiple = true;
-    if (quest.category && quest.question && quest.answer && quest.wrong1 && quest.wrong2) {
-      payload.category = quest.category;
-      payload.question = quest.question;
-      payload.right_answer = quest.answer;
-      payload.wrong_answers = [ quest.wrong1, quest.wrong2, quest.wrong3 ]
-      console.log(payload);
-      Quest.create(payload);
-      return res.redirect('/');
-    }
-  } else {
-    payload.question = quest.statement;
-    payload.isMultiple = false;
-    if (quest.right == 'true') {
-      payload.isTrue = true;
-    } else {
-      payload.isTrue = false;
-    }
+  payload.isMultiple = true;
+  if (quest.category && quest.question && quest.answer && quest.wrong1 && quest.wrong2) {
+    payload.category = quest.category;
+    payload.question = quest.question;
+    payload.right_answer = quest.answer;
+    payload.wrong_answers = [ quest.wrong1, quest.wrong2, quest.wrong3 ]
     console.log(payload);
     Quest.create(payload);
     return res.redirect('/');
-}
+  }
 })
 
 
